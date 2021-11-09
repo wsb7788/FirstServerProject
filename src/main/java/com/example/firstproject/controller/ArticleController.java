7b4482ec9b1a9064539cdd284f16f894e1c6fca0
wsb7788 <wsb0514@naver.com>
2021-11-10 04:03:48 +0900

    form data jpa를 이용해 저장 후 출력

--- src/main/java/com/example/firstproject/controller/ArticleController.java
+++ src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -1,12 +1,20 @@
 package com.example.firstproject.controller;
 
 import com.example.firstproject.dto.ArticleForm;
+import com.example.firstproject.entity.Article;
+import com.example.firstproject.repository.ArticleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ArticleController {
+    @Autowired // 스프링부트가 미리 생성해놓은 객체를 가져다가 자동 연결!
+    private ArticleRepository articleRepository;
+
+
+
     @GetMapping("/articles/new")
     public String newArticleForm(){
 
@@ -16,6 +24,16 @@ public class ArticleController {
     @PostMapping("/articles/create")
     public String createArticle(ArticleForm form){
         System.out.println(form.toString());
+
+        // 1. DTO 를Entity로 변환
+        Article article = form.toEntity();
+        System.out.println(article.toString());
+
+        // 2. repository에게 entity를 DB안에 저장하게 함
+        Article saved = articleRepository.save(article);
+        System.out.println(saved.toString());
+
+
         return "";
 
     }
