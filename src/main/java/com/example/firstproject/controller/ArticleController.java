3108a2a81e634327ebf63acabf539816b702c60c
wsb7788 <wsb0514@naver.com>
2021-11-10 05:30:18 +0900

    lombok을 활용한 리펙토링

--- src/main/java/com/example/firstproject/controller/ArticleController.java
+++ src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -3,12 +3,14 @@ package com.example.firstproject.controller;
 import com.example.firstproject.dto.ArticleForm;
 import com.example.firstproject.entity.Article;
 import com.example.firstproject.repository.ArticleRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
+@Slf4j  // 로깅을위한 annotation
 public class ArticleController {
     @Autowired // 스프링부트가 미리 생성해놓은 객체를 가져다가 자동 연결!
     private ArticleRepository articleRepository;
@@ -23,16 +25,18 @@ public class ArticleController {
     }
     @PostMapping("/articles/create")
     public String createArticle(ArticleForm form){
-        System.out.println(form.toString());
+        // System.out.println(form.toString());  ->  log로 대체
+        log.info(form.toString());
+
 
         // 1. DTO 를Entity로 변환
         Article article = form.toEntity();
-        System.out.println(article.toString());
-
+       // System.out.println(article.toString());
+        log.info(article.toString());
         // 2. repository에게 entity를 DB안에 저장하게 함
         Article saved = articleRepository.save(article);
-        System.out.println(saved.toString());
-
+        //System.out.println(saved.toString());
+        log.info(saved.toString());
 
         return "";
 
--- src/main/java/com/example/firstproject/entity/Article.java
+++ src/main/java/com/example/firstproject/entity/Article.java
@@ -1,11 +1,16 @@
 package com.example.firstproject.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.ToString;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity  //DB가 해당 객체를 인식 가능
+@AllArgsConstructor
+@ToString
 public class Article {
 
     @Id  //대표 값 like 주민번호
@@ -19,18 +24,6 @@ public class Article {
     private String content;
 
 
-    @Override
-    public String toString() {
-        return "Article{" +
-                "id=" + id +
-                ", title='" + title + '\'' +
-                ", content='" + content + '\'' +
-                '}';
-    }
 
-    public Article(Long id, String title, String content) {
-        this.id = id;
-        this.title = title;
-        this.content = content;
-    }
+
 }
